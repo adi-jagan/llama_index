3c422f0b057888f5ba51566689dfb0b53e554e4e
Adi Jagnnathan <Aditya.Jagnnathan@gmail.com>
2023-12-10 13:11:31 -0800

    5 line example implementation

--- starter.py
+++ starter.py
@@ -4,5 +4,5 @@ documents = SimpleDirectoryReader("data").load_data()
 index = VectorStoreIndex.from_documents(documents)
 
 query_engine = index.as_query_engine()
-response = query_engine.query("What did the author do growing up? Write 3 sentences in bulletpoints & a headline to encapsulate the theme of the essay")
+response = query_engine.query("Write 3 sentences in bulletpoints & a headline to encapsulate the theme of the essay")
 print(response)
